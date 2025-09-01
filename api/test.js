97310c6eb719525603e4f8022daad0494798915e
Hriddha19 <s2gedits10k@gmail.com>
2025-09-01 11:58:50 +0530

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,9 @@
+// api/test.js
+export default function handler(req, res) {
+  res.status(200).json({ 
+    message: 'API is working!',
+    method: req.method,
+    url: req.url,
+    query: req.query
+  });
+}
